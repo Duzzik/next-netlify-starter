1902b3d855a6d045169f5561462f2450a0a92f75
Duzzik <104212834+Duzzik@users.noreply.github.com>
2022-05-11 17:01:29 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          "Don't touch this!" <code>pages/index.js</code>
+          Don\'t touch this! <code>pages/index.js</code>
         </p>
       </main>
 
